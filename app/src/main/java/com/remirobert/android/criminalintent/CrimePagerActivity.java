f3edaff2b1dccefe553fc10fb76561f0df49518a
rémi  <remirobert33530@gmail.com>
2016-11-11 17:21:04 +0100

    add pager view and layout

--- app/src/main/java/com/remirobert/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/remirobert/android/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.remirobert.android.criminalintent;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v4.app.FragmentActivity;
 
 /**
@@ -7,4 +9,10 @@ import android.support.v4.app.FragmentActivity;
  */
 
 public class CrimePagerActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_crime_pager);
+    }
 }
