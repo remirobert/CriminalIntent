1b8bd4019881a538fc111308e546eb6d83afa630
rémi  <remirobert33530@gmail.com>
2016-11-15 08:58:46 +0100

    add pager view instance

--- app/src/main/java/com/remirobert/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/remirobert/android/criminalintent/CrimePagerActivity.java
@@ -1,8 +1,14 @@
 package com.remirobert.android.criminalintent;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
 
 /**
  * Created by remirobert on 11/11/2016.
@@ -10,9 +16,30 @@ import android.support.v4.app.FragmentActivity;
 
 public class CrimePagerActivity extends FragmentActivity {
 
+    private ViewPager mViewPager;
+    private List<Crime> mCrimeList;
+
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_crime_pager);
+
+        mCrimeList = CrimeLab.get().getCrimeList();
+
+        mViewPager = (ViewPager)findViewById(R.id.activity_crime_pager_view_pager);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimeList.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimeList.size();
+            }
+        });
     }
 }
